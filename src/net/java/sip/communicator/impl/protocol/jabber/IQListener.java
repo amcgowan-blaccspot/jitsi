d128b88c2301581c3bbd7452f33d0aff2500709f
amcgowan-blaccspot <amcgowan@blaccspot.com>
2019-02-11 14:09:44 -0800

    Rename listeners

--- src/net/java/sip/communicator/impl/protocol/jabber/IQListener.java
+++ src/net/java/sip/communicator/impl/protocol/jabber/IQListener.java
@@ -4,6 +4,6 @@ package net.java.sip.communicator.impl.protocol.jabber;
 import net.java.sip.communicator.impl.protocol.jabber.extensions.jingle.JingleIQ;
 import org.jivesoftware.smack.*;
 
-public abstract class JingleListener {
-    public abstract JingleIQ invoke(JingleIQ iq, XMPPConnection connection);
+public abstract class IQListener {
+    public abstract IQ invoke(IQ iq, XMPPConnection connection);
 }
